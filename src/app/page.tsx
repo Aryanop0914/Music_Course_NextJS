321521122e871a4999b934dbe016220365d9a64c
Aryanop0914 <21cs038@charusat.edu.in>
2024-02-27 21:36:19 +0530

    Instructor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonal from "@/components/TestimonalCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -12,6 +13,7 @@ export default function Home() {
       <WhyChooseUs />
       <MusicSchoolTestimonal />
       <UpcomingWebinars />
+      <Instructors />
     </main>
   );
 }
