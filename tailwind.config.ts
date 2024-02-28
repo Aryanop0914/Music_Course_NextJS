f0f656b43273ffd0bdcdffb1f519b656c9ff5425
Aryanop0914 <21cs038@charusat.edu.in>
2024-02-28 20:35:50 +0530

    contact us and courses page done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,10 +73,18 @@ const config: Config = {
             transform: "translate(calc(-50% - 0.5rem))",
           },
         },
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
+          },
+        },
       },
     },
+    plugins: [addVariablesForColors, addSvgPatterns],
   },
-  plugins: [addVariablesForColors, addSvgPatterns],
 };
 
 export default config;
